554a4956ab78b9d6ebb7bbf3609628d8219ba0bd
Isadora Ávila <isadorgravila@gmail.com>
2022-09-21 10:58:12 -0300

    test backend: POST - upvote and downvote - check status 200 and 404

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -1,17 +1,19 @@
 import app from '../src/app.js';
 import supertest from 'supertest';
 import { prisma } from "../src/database";
-import { recommendationBody } from './factories/recommendationFactory.js';
+import * as recommendationFactory from './factories/recommendationFactory.js';
 
 beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE recommendations RESTART IDENTITY;`;
 });
 
+const agent = supertest(app);
+
 describe("POST /recommendations", () => {
     it("returns 201 for valid input and right insert in the database", async () => {
-        const music = await recommendationBody();
+        const music = await recommendationFactory.recommendation();
 
-        const result = await supertest(app).post('/recommendations').send(music);
+        const result = await agent.post('/recommendations').send(music);
 
         const createdRecommendation = await prisma.recommendation.findUnique({
             where: { name: music.name }
@@ -22,20 +24,82 @@ describe("POST /recommendations", () => {
     });
 
     it("returns 409 for using an existing music name in the database", async () => {
-        const music = await recommendationBody();
+        const music = await recommendationFactory.recommendation();
 
-        await supertest(app).post('/recommendations').send(music);
+        await agent.post('/recommendations').send(music);
 
-        const result = await supertest(app).post('/recommendations').send(music);
+        const result = await agent.post('/recommendations').send(music);
 
         expect(result.status).toBe(409);
     });
 
     it("returns 422 for invalid input", async () => {
-        const result = await supertest(app).post('/recommendations').send({});
+        const result = await agent.post('/recommendations').send({});
 
         expect(result.status).toBe(422);
     });
+});
+
+describe('POST /recommendations/:id/upvote', () => {
+    it('returns 404 for id not found', async () => {
+        const id = 1000;
+
+        const result = await agent.post(`/recommendations/${id}/upvote`).send({});
+
+        expect(result.status).toBe(404);
+    });
+
+    it('returns 200 for success in upvote', async () => {
+        const music = await recommendationFactory.create();
+        const { id, score } = music;
+
+        const result = await agent.post(`/recommendations/${id}/upvote`).send({});
+
+        const recommendation = await prisma.recommendation.findUnique({
+            where: { id }
+        });
+
+        expect(result.status).toBe(200);
+        expect(recommendation.score).toBe(score + 1);
+    });
+});
+
+describe('POST /recommendations/:id/downvote', () => {
+    it('returns 404 for id not found', async () => {
+        const id = 1000;
+
+        const result = await agent.post(`/recommendations/${id}/downvote`).send({});
+
+        expect(result.status).toBe(404);
+    });
+    
+    it('returns 200 for success in downvote', async () => {
+        const music = await recommendationFactory.create();
+        const { id, score } = music;
+
+        const result = await agent.post(`/recommendations/${id}/downvote`).send({});
+
+        const recommendation = await prisma.recommendation.findUnique({
+            where: { id }
+        });
+
+        expect(result.status).toBe(200);
+        expect(recommendation.score).toBe(score - 1);
+    });
+
+    it('returns 200 and delete recommendation when score gets lower then -5', async () => {
+        const music = await recommendationFactory.createWithLowScore();
+        const { id } = music;
+
+        const result = await agent.post(`/recommendations/${id}/downvote`).send({});
+
+        const recommendation = await prisma.recommendation.findUnique({
+            where: { id }
+        });
+
+        expect(result.status).toBe(200);
+        expect(recommendation).toBeNull;
+    })
 });
 
 afterAll(async () => {
--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,8 +1,27 @@
 import { faker } from '@faker-js/faker';
+import { prisma } from "../../src/database";
 
-export async function recommendationBody() {
+export async function recommendation() {
     return {
         name: faker.lorem.words(5),
         youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
     }
+};
+
+export async function create() {
+    const body = await recommendation();
+
+    const createdRecommendation = await prisma.recommendation.create({
+        data: body
+    });
+    return createdRecommendation;
+}
+
+export async function createWithLowScore() {
+    const body = await recommendation();
+
+    const createdRecommendation = await prisma.recommendation.create({
+        data: { ...body, score: -5 }
+    });
+    return createdRecommendation;
 }
