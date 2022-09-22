823453b7921c977d54b378d76dc02a97a9d854b3
Isadora Ávila <isadorgravila@gmail.com>
2022-09-22 11:12:03 -0300

    create route of reset database when testing

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,12 +1,20 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import dotenv from 'dotenv';
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 import recommendationRouter from "./routers/recommendationRouter.js";
+import e2eRouter from "./routers/e2eRouter.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'test') {
+    app.use(e2eRouter);
+}
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
