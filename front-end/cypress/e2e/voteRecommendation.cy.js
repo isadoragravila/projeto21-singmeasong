740d0194bffe0efd18f77ad724e83212ea0a53ea
Isadora Ávila <isadorgravila@gmail.com>
2022-09-24 18:43:38 -0300

    test e2e: tests top page when downvoted and upvoted

--- front-end/cypress/e2e/voteRecommendation.cy.js
+++ front-end/cypress/e2e/voteRecommendation.cy.js
@@ -7,13 +7,9 @@ beforeEach(async () => {
 
 describe('Vote recommendation', () => {
   it('upvote: should increase score in 1', () => {
-    cy.visit(URL_FRONT);
-
-    cy.intercept('GET', `${URL_BACK}/recommendations`).as('getRecommendations');
-
     cy.createRecommendation(URL_BACK);
 
-    cy.wait('@getRecommendations');
+    cy.visit(URL_FRONT);
 
     cy.get("[data-cy=upvote]").click();
 
@@ -21,13 +17,9 @@ describe('Vote recommendation', () => {
   });
   
   it('downvote: should decrease score in 1', () => {
-    cy.visit(URL_FRONT);
-
-    cy.intercept('GET', `${URL_BACK}/recommendations`).as('getRecommendations');
-
     cy.createRecommendation(URL_BACK);
 
-    cy.wait('@getRecommendations');
+    cy.visit(URL_FRONT);
 
     cy.get("[data-cy=downvote]").click();
 
@@ -35,9 +27,9 @@ describe('Vote recommendation', () => {
   });
 
   it('downvote: should delete recommendation when score is less than -5', () => {
-    cy.visit(URL_FRONT);
-
     cy.createRecommendation(URL_BACK);
+    
+    cy.visit(URL_FRONT);
 
     cy.get("[data-cy=downvote]").click();
     cy.wait(500);
@@ -53,5 +45,43 @@ describe('Vote recommendation', () => {
     cy.wait(500);
 
     cy.get("[data-cy=emptyRecommendations]").should("be.visible");
-  })
+  });
+
+  it('on page top, the first recommendation should go down when downvoted', () => {
+    cy.createRecommendation(URL_BACK);
+    cy.createRecommendation(URL_BACK);
+    
+    cy.visit(`${URL_FRONT}/top`);
+
+    cy.get("[data-cy=title]").eq(0).then((title) => {
+      const txt = title.text();
+
+      cy.intercept('GET', `${URL_BACK}/recommendations/top/10`).as('getTopRecommendations');
+  
+      cy.get("[data-cy=downvote]").eq(0).click();
+  
+      cy.wait('@getTopRecommendations');
+  
+      cy.get("[data-cy=title]").eq(0).should('not.have.value', txt);
+    });
+  });
+
+  it('on page top, the second recommendation should go up when upvoted', () => {
+    cy.createRecommendation(URL_BACK);
+    cy.createRecommendation(URL_BACK);
+    
+    cy.visit(`${URL_FRONT}/top`);
+
+    cy.get("[data-cy=title]").eq(0).then((title) => {
+      const txt = title.text();
+
+      cy.intercept('GET', `${URL_BACK}/recommendations/top/10`).as('getTopRecommendations');
+  
+      cy.get("[data-cy=upvote]").eq(1).click();
+  
+      cy.wait('@getTopRecommendations');
+  
+      cy.get("[data-cy=title]").eq(0).should('not.have.value', txt);
+    });
+  });
 });
